4a03d8c54ee20f67672c5199098ae7a82db34807
Manish Bhurtel <135348953+bhurtelmanish@users.noreply.github.com>
2024-11-14 19:06:45 -0600

    Update ImageDetails.jsx

--- src/Components/ImageDetails.jsx
+++ src/Components/ImageDetails.jsx
@@ -4,6 +4,7 @@ import { FaArrowLeft } from "react-icons/fa6";
 
 const ImageDetails = () => {
   return (
+    //This file is not being used in the project ----
     <div className="dark:bg-[var(--darkBackground)] dark:text-[var(--lightColor)] h-screen px-4">
         <FaArrowLeft className='text-2xl' />
       Image Details on this page
