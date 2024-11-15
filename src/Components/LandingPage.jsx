20be9ef345c056256fde17c714e57fa0e8f5232a
Manish Bhurtel <135348953+bhurtelmanish@users.noreply.github.com>
2024-11-14 19:11:55 -0600

    Update LandingPage.jsx

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -8,7 +8,7 @@ const LandingPage = () => {
   return (
     <div className='flex  flex-col h-auto dark:bg-[var(--darkBackground)] dark:text-[var(--lightColor)] z-[-1] pt-6 px-4 flex'>
       <div className='flex flex-col md:justify-center md:items-center'>
-        <header className='font-extrabold md:mt-5  md:text-[3.5rem] text-[2.6rem] text-[var(--themeColor)]'><span className='text-[var(--blueColor)]'>Pixabay</span> Image Gallery</header>
+        <header className='font-extrabold md:mt-5  md:text-[3.5rem] text-[2.6rem] text-[var(--themeColor)] leading-tight'><span className='text-[var(--blueColor)]'>Pixabay</span> Image Gallery</header>
         <p className='mt-3 mb-4 text-justify md:text-center text-md leading-5 md:w-[80%] dark:text-[var(--lightGrayColor)] text-[var(--grayColor)] '>Welcome to <span className='text-[var(--pinkColor)] font-[500]'>{API_NAME} </span> image gallery. It contains all the photos and has a very advanced search query. Search all the pictures you need by clicking on link from the navigation bar.</p>
       </div>
 
